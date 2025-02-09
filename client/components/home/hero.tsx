330e6f7ec85a66a2051eabe3a7116040672718f3
Tyler Maksymiw <tylermcodes@gmail.com>
2025-02-09 11:53:54 -0500

    feat(hero): Add hero headline and subtext

--- client/components/home/hero.tsx
+++ client/components/home/hero.tsx
@@ -1,7 +1,8 @@
 export default function Hero() {
     return (   
-        <div className="w-full">
-            <h1>Hello World</h1>
+        <div className="w-full py-48">
+            <h1 className="text-7xl font-bold w-1/2 pb-8">Find your next game obsession</h1>
+            <p className="w-2/5 text-lg"> Dive into a world built by gamers, for gamers. Write reviews, discover hidden gems, and connect with a vibrant community who share your passion for all things gaming </p>
         </div>
     );
 }
